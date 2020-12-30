4f306fa6bcf6c1d438ae7b427e322a0100e581e7
Triet Le <letrongtriet@gmail.com>
2020-12-30 18:42:26 +0200

    Initial Commit

---
+++ Zilando/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Zilando
+//
+//  Created by Le, Triet on 30.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
